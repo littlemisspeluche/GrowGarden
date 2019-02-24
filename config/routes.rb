7993b761bc7a6560c7011acbd6dd1753d63d81e9
orianne gilaad <shaharorian@gmail.com>
2019-02-24 16:09:39 +0200

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :categories, only: [:index, :show] do
+    resources :species, only: [:index, :show] do
+      resources :plants, only: [:index, :show]
+    end
+  end
+
+  resources :garden_plants
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
