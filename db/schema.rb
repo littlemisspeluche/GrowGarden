113b062d8ea9a5c5c4f9e703eac3a66836b7f614
Barak <barakbbg@gmail.com>
2019-02-26 19:38:07 +0200

    form change

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_02_26_161218) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_161218) do
   create_table "plants", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.text "requirements"
     t.bigint "species_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
