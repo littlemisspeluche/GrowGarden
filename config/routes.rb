8beb8873ca0a6fcc426f035610fc4b9565526779
liad peisic <liad1570@walla.com>
2019-02-26 10:56:56 +0200

    newHome

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'pages#home'
+  root to: 'categories#index'
 
   resources :categories, only: [:index, :show] do
     resources :species, only: [:index, :show] do
