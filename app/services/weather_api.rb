e92f77b36dee13040bfb6314ac229761bb8ce569
yaniv matalon <matalonyaniv14@gmail.com>
2019-02-28 05:19:28 -0500

    class wetaher api fo getting and seeting weather and light info, class overall health for comparing requirements to status set by weatherapi class

--- app/services/weather_api.rb
+++ app/services/weather_api.rb
@@ -1,18 +1,54 @@
 class WeatherApi
   def initialize(options = {})
-    @plant = options[:plant]
+    @garden = options[:garden]
+    configure
   end
 
+
   def perform
-    check_weather
+    setting_daily_temp(@garden)
+    getting_daily_light(@garden)
+    setting_daily_light(@garden)
   end
 
   private
 
-  def check_weather
-    puts "Connecting to OpenWeather..."
-    sleep(2)
-    puts "Here's the wether for today..."
+  def setting_daily_temp(garden)
+    puts "Setting Daily Temperature"
+    today_temp = Openweather2.get_weather(city: "#{garden.location}", units: 'metric')
+    garden.status.daily_temp = today_temp.temperature
+    garden.status.save
+    puts "Saved"
+  end
+
+  def getting_daily_light(garden)
+    puts "Getting Daily Light......"
+    today_light = Openweather2.get_weather(city: "#{garden.location}", units: 'metric')
+    garden.status.daily_light = today_light.clouds
+    garden.status.save
+    puts 'Saved'
+  end
+
+  def setting_daily_light(garden)
+    puts "Setting Daily Light......"
+    if garden.status.daily_light.to_i < 20
+      garden.status.daily_light = "Full Sun"
+    elsif garden.status.daily_light.to_i > 70
+      garden.status.daily_light = "Partial Shade"
+    else
+      garden.status.daily_light = "Full Sun to Partial Shade"
+    end
+    garden.status.save
+    puts 'Saved'
+  end
+
+
+  def configure
+    puts 'setting API Key'
+    Openweather2.configure do |config|
+      config.endpoint = 'http://api.openweathermap.org/data/2.5/weather'
+      config.apikey = "7522ff79b2ce460c78e068e593355180"
+    end
   end
 
 end
