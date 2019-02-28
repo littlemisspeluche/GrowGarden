c35ebf4fc387558dff7f94de5557e1b4f0d03d55
yaniv matalon <matalonyaniv14@gmail.com>
2019-02-28 07:10:04 -0500

    small changes to overal healh

--- app/services/overall_health.rb
+++ app/services/overall_health.rb
@@ -5,19 +5,24 @@ class OverallHealth
   end
 
   def perform
+    puts 'Checking Weather Requirements'
+    check_weather(@garden)
+    puts 'Checking Light Requirements'
+    check_light(@garden)
+    puts "Overall Health: #{@score}"
   end
 
   private
   def check_weather(garden)
     min_temp = garden.plant.requirement.temperature.split[0].to_f
     max_temp = garden.plant.requirement.temperature.split[2].to_f
-    today_temp < min_temp || today_temp > max_temp ? @score += 1 : @score += 0
+    garden.status.daily_temp.to_f < min_temp || garden.status.daily_temp.to_f > max_temp ? @score += 1 : @score += 0
   end
 
   def check_light(garden)
     light_status = garden.status.daily_light
     light_requirement = garden.plant.requirement.light
-    light_status.include?(light_requirement) ? @score += 0 : @score += 1
+    light_requirement.include?(light_status) ? @score += 0 : @score += 1
   end
 
   def check_water
