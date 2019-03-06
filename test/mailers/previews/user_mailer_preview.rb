ed566cb97b8456b1b26c88a1aecf963524248d02
orianne gilaad <shaharorian@gmail.com>
2019-03-06 13:08:07 +0200

    added mailers base style

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,6 +1,16 @@
 class UserMailerPreview < ActionMailer::Preview
+  def welcome
+    user = User.first
+    UserMailer.welcome(user)
+  end
+
+  def plant_added
+    user = User.first
+    UserMailer.plant_added(user)
+  end
+
   def daily
     user = User.first
-    UserMailer.daily(user)
+    UserMailer.daily_report(user)
   end
 end
